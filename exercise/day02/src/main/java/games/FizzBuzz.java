0ffe03fc766579686a75c242c40f1e776c8c5da1
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-02 13:08:30 +0100

    Extracted isFizz() and isBuzz()

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -8,9 +8,17 @@ public class FizzBuzz {
         if (input <= 0) throw new OutOfRangeException();
         if (input > 100) throw new OutOfRangeException();
 
-        if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
-        if (input % 3 == 0) return "Fizz";
-        if (input % 5 == 0) return "Buzz";
+        if (isFizz(input) && isBuzz(input)) return "FizzBuzz";
+        if (isFizz(input)) return "Fizz";
+        if (isBuzz(input)) return "Buzz";
         else return input.toString();
+    }
+
+    private static boolean isFizz(Integer input) {
+        return input % 3 == 0;
+    }
+
+    private static boolean isBuzz(Integer input) {
+        return input % 5 == 0;
     }
 }
