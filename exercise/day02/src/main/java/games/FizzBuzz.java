aa0327cf2e6b455d42937eba40958954aad200f3
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-02 13:07:29 +0100

    Removed braces for oneliners

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -8,15 +8,9 @@ public class FizzBuzz {
         if (input <= 0) throw new OutOfRangeException();
         if (input > 100) throw new OutOfRangeException();
 
-        if (input % 3 == 0 && input % 5 == 0) {
-            return "FizzBuzz";
-        }
-        if (input % 3 == 0) {
-            return "Fizz";
-        }
-        if (input % 5 == 0) {
-            return "Buzz";
-        }
-        return input.toString();
+        if (input % 3 == 0 && input % 5 == 0) return "FizzBuzz";
+        if (input % 3 == 0) return "Fizz";
+        if (input % 5 == 0) return "Buzz";
+        else return input.toString();
     }
 }
