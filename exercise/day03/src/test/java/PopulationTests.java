b23ad5c85eb05a1685ad1cfc5c1fb04d0e4107e2
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-03 14:07:49 +0100

    Assert on actual Person

--- exercise/day03/src/test/java/PopulationTests.java
+++ exercise/day03/src/test/java/PopulationTests.java
@@ -8,36 +8,34 @@ import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PopulationTests {
-    private static People people;
+    Person peter = new Person("Peter", "Griffin")
+        .addPet(PetType.CAT, "Tabby", 2);
+    Person stewie = new Person("Stewie", "Griffin")
+        .addPet(PetType.CAT, "Dolly", 3)
+        .addPet(PetType.DOG, "Brian", 9);
+    Person joe = new Person("Joe", "Swanson")
+        .addPet(PetType.DOG, "Spike", 4);
+    Person lois = new Person("Lois", "Griffin")
+        .addPet(PetType.SNAKE, "Serpy", 1);
+    Person meg = new Person("Meg", "Griffin")
+        .addPet(PetType.BIRD, "Tweety", 1);
+    Person chris = new Person("Chris", "Griffin")
+        .addPet(PetType.TURTLE, "Speedy", 4);
+    Person cleveland = new Person("Cleveland", "Brown")
+        .addPet(PetType.HAMSTER, "Fuzzy", 1)
+        .addPet(PetType.HAMSTER, "Wuzzy", 2);
+    Person glenn = new Person("Glenn", "Quagmire");
 
     @BeforeAll
     static void setup() {
-        people = new People(
-                new Person("Peter", "Griffin")
-                        .addPet(PetType.CAT, "Tabby", 2),
-                new Person("Stewie", "Griffin")
-                        .addPet(PetType.CAT, "Dolly", 3)
-                        .addPet(PetType.DOG, "Brian", 9),
-                new Person("Joe", "Swanson")
-                        .addPet(PetType.DOG, "Spike", 4),
-                new Person("Lois", "Griffin")
-                        .addPet(PetType.SNAKE, "Serpy", 1),
-                new Person("Meg", "Griffin")
-                        .addPet(PetType.BIRD, "Tweety", 1),
-                new Person("Chris", "Griffin")
-                        .addPet(PetType.TURTLE, "Speedy", 4),
-                new Person("Cleveland", "Brown")
-                        .addPet(PetType.HAMSTER, "Fuzzy", 1)
-                        .addPet(PetType.HAMSTER, "Wuzzy", 2),
-                new Person("Glenn", "Quagmire")
-        );
     }
 
     @Test
     void whoOwnsTheYoungestPet() {
+        People people = new People(peter, stewie, joe, lois, meg, chris, cleveland, glenn);
         var filtered = people.youngestPetOwner();
 
         assert filtered != null;
-        assertThat(filtered.firstName()).isEqualTo("Lois");
+        assertThat(filtered).isEqualTo(lois);
     }
 }
