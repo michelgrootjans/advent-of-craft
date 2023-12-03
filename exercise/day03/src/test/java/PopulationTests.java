2c5e41899c7123f770d8253e3ba992b3ea17f655
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-03 14:40:24 +0100

    Switched tests to snake-case

--- exercise/day03/src/test/java/PopulationTests.java
+++ exercise/day03/src/test/java/PopulationTests.java
@@ -9,12 +9,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PopulationTests {
     @Test
-    void noPeopleReturnsNoValue() {
+    void no_people_returns_no_value() {
         assertThat(new People().youngestPetOwner()).isEmpty();
     }
 
     @Test
-    void personWithoutPets() {
+    void person_without_pets() {
         Person glenn = new Person("Glenn", "Quagmire");
         assertThat(new People(glenn).youngestPetOwner()).hasValue(glenn);
         // this needs to be discussed with stakeholders. This could make more sense:
@@ -22,13 +22,13 @@ class PopulationTests {
     }
 
     @Test
-    void onePersonWithPet() {
+    void one_person_with_pet() {
         Person peter = new Person("Peter", "Griffin").addPet(PetType.CAT, "Tabby", 2);
         assertThat(new People(peter).youngestPetOwner()).hasValue(peter);
     }
 
     @Test
-    void twoPeopleWithPetsOfDifferentAge() {
+    void two_people_with_pets_of_different_age() {
         Person peter = new Person("Peter", "Griffin").addPet(PetType.CAT, "Tabby", 2);
         Person lois = new Person("Lois", "Griffin").addPet(PetType.SNAKE, "Serpy", 1);
         assertThat(new People(peter, lois).youngestPetOwner()).hasValue(lois);
@@ -36,7 +36,7 @@ class PopulationTests {
     }
 
     @Test
-    void twoPeopleWithPetsOfSameAge() {
+    void two_people_with_pets_of_same_age() {
         Person meg = new Person("Meg", "Griffin").addPet(PetType.BIRD, "Tweety", 1);
         Person lois = new Person("Lois", "Griffin").addPet(PetType.SNAKE, "Serpy", 1);
         assertThat(new People(meg, lois).youngestPetOwner()).hasValue(meg);
@@ -44,7 +44,7 @@ class PopulationTests {
     }
 
     @Test
-    void whoOwnsTheYoungestPet() {
+    void who_owns_the_youngest_pet() {
         Person peter = new Person("Peter", "Griffin")
             .addPet(PetType.CAT, "Tabby", 2);
         Person stewie = new Person("Stewie", "Griffin")
