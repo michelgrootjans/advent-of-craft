0534644ed1064e7177afeb59da143b211c473029
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-07 15:46:46 +0100

    renamed log to logger

--- exercise/day07/src/main/java/ci/Pipeline.java
+++ exercise/day07/src/main/java/ci/Pipeline.java
@@ -8,46 +8,46 @@ import ci.dependencies.Project;
 public class Pipeline {
     private final Config config;
     private final Emailer emailer;
-    private final Logger log;
+    private final Logger logger;
 
-    public Pipeline(Config config, Emailer emailer, Logger log) {
+    public Pipeline(Config config, Emailer emailer, Logger logger) {
         this.config = config;
         this.emailer = emailer;
-        this.log = log;
+        this.logger = logger;
     }
 
     public void run(Project project) {
         if (project.hasTests()) {
             if (project.runTests().equals("success")) {
-                log.info("Tests passed");
+                logger.info("Tests passed");
                 deploy(project);
             } else {
-                log.error("Tests failed");
+                logger.error("Tests failed");
 
                 sendMail("Tests failed");
             }
         } else {
-            log.info("No tests");
+            logger.info("No tests");
             deploy(project);
         }
     }
 
     private void deploy(Project project) {
         if (project.deploy().equals("success")) {
-            log.info("Deployment successful");
+            logger.info("Deployment successful");
             sendMail("Deployment completed successfully");
         } else {
-            log.error("Deployment failed");
+            logger.error("Deployment failed");
             sendMail("Deployment failed");
         }
     }
 
     private void sendMail(String Deployment_completed_successfully) {
         if (config.sendEmailSummary()) {
-            log.info("Sending email");
+            logger.info("Sending email");
             emailer.send(Deployment_completed_successfully);
         } else {
-            log.info("Email disabled");
+            logger.info("Email disabled");
         }
     }
 }
