db700f68a5147d21a648919a0f63bf3449e18dc3
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-07 15:41:08 +0100

    removed unused lines

--- exercise/day07/src/main/java/ci/Pipeline.java
+++ exercise/day07/src/main/java/ci/Pipeline.java
@@ -18,66 +18,33 @@ public class Pipeline {
 
     public void run(Project project) {
         if (project.hasTests()) {
-            boolean testsPassed;
-            boolean deploySuccessful;
 
             if (project.runTests().equals("success")) {
                 log.info("Tests passed");
-                testsPassed = true;
-                boolean deploySuccessful1;
-                if (testsPassed) {
-                    if ("success".equals(project.deploy())) {
-                        log.info("Deployment successful");
-                        deploySuccessful1 = true;
-                    } else {
-                        log.error("Deployment failed");
-                        deploySuccessful1 = false;
-                    }
+                boolean deploySuccessful1 = false;
+                if ("success".equals(project.deploy())) {
+                    log.info("Deployment successful");
+                    deploySuccessful1 = true;
                 } else {
-                    deploySuccessful1 = false;
+                    log.error("Deployment failed");
                 }
 
                 if (config.sendEmailSummary()) {
                     log.info("Sending email");
-                    if (testsPassed) {
-                        if (deploySuccessful1) {
-                            emailer.send("Deployment completed successfully");
-                        } else {
-                            emailer.send("Deployment failed");
-                        }
+                    if (deploySuccessful1) {
+                        emailer.send("Deployment completed successfully");
                     } else {
-                        emailer.send("Tests failed");
+                        emailer.send("Deployment failed");
                     }
                 } else {
                     log.info("Email disabled");
                 }
             } else {
                 log.error("Tests failed");
-                testsPassed = false;
-                boolean deploySuccessful1;
-                if (testsPassed) {
-                    if ("success".equals(project.deploy())) {
-                        log.info("Deployment successful");
-                        deploySuccessful1 = true;
-                    } else {
-                        log.error("Deployment failed");
-                        deploySuccessful1 = false;
-                    }
-                } else {
-                    deploySuccessful1 = false;
-                }
 
                 if (config.sendEmailSummary()) {
                     log.info("Sending email");
-                    if (testsPassed) {
-                        if (deploySuccessful1) {
-                            emailer.send("Deployment completed successfully");
-                        } else {
-                            emailer.send("Deployment failed");
-                        }
-                    } else {
-                        emailer.send("Tests failed");
-                    }
+                    emailer.send("Tests failed");
                 } else {
                     log.info("Email disabled");
                 }
