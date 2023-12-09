a61f416cd8eebcd6b805607dc8a69aa9a21f7ace
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-09 11:35:06 +0100

    Validate on uppercase

--- exercise/day08/src/test/java/PasswordtValidationTest.java
+++ exercise/day08/src/test/java/PasswordtValidationTest.java
@@ -2,6 +2,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class PasswordtValidationTest {
     @Test
     void valid_password() {
@@ -13,9 +17,25 @@ class PasswordtValidationTest {
         assertThat(isValid("ABCde1*")).isFalse();
     }
 
+    @Test
+    void no_uppercase() {
+        assertThat(isValid("abcdef1*")).isFalse();
+    }
+
     private boolean isValid(String password) {
         if (password.length() < 8) return false;
+        if(failsUppercaseRule(password)) return false;
 
         return true;
+    }
+
+    private boolean failsUppercaseRule(String password) {
+        List<String> passwordLetters = toLetters(password);
+        List<String> uppercaseAlphabet = toLetters("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        return passwordLetters.stream().noneMatch(uppercaseAlphabet::contains);
+    }
+
+    private ArrayList<String> toLetters(String password) {
+        return new ArrayList<>(Arrays.asList(password.split("")));
     }
 }
