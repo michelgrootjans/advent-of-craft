1a78699ba365286c98f2a4a8992b960f114bcad8
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-09 11:40:09 +0100

    Validate on special characters

--- exercise/day08/src/test/java/PasswordtValidationTest.java
+++ exercise/day08/src/test/java/PasswordtValidationTest.java
@@ -32,11 +32,17 @@ class PasswordtValidationTest {
         assertThat(isValid("ABCdefg*")).isFalse();
     }
 
+    @Test
+    void no_special_character() {
+        assertThat(isValid("ABCdef1h")).isFalse();
+    }
+
     private boolean isValid(String password) {
         if (password.length() < 8) return false;
         if(failsUppercaseRule(password)) return false;
         if(failsLowercaseRule(password)) return false;
         if(failsNumbersRule(password)) return false;
+        if(failsSpecialCharacterRule(password)) return false;
 
         return true;
     }
@@ -57,6 +63,12 @@ class PasswordtValidationTest {
         List<String> passwordLetters = toLetters(password);
         List<String> allowedNumbers = toLetters("0123456789");
         return passwordLetters.stream().noneMatch(allowedNumbers::contains);
+    }
+
+    private boolean failsSpecialCharacterRule(String password) {
+        List<String> passwordLetters = toLetters(password);
+        List<String> specialCharacters = toLetters(".*#@$%&");
+        return passwordLetters.stream().noneMatch(specialCharacters::contains);
     }
 
     private ArrayList<String> toLetters(String password) {
