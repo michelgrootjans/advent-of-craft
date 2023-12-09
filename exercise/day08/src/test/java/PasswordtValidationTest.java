8db513aeca544560cdcaf074a5a5952a62ef7240
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-09 12:01:21 +0100

    Added Property based tests

--- exercise/day08/src/test/java/PasswordtValidationTest.java
+++ exercise/day08/src/test/java/PasswordtValidationTest.java
@@ -1,5 +1,12 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.Chars;
+import net.jqwik.api.constraints.LowerChars;
+import net.jqwik.api.constraints.NotBlank;
+import net.jqwik.api.constraints.NumericChars;
+import net.jqwik.api.constraints.UpperChars;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,34 +14,56 @@ import java.util.Arrays;
 import java.util.List;
 
 class PasswordtValidationTest {
+    private final String valid_password = "ABCdef1*";
+
     @Test
     void valid_password() {
-        assertThat(isValid("ABCdef1*")).isTrue();
+        assertThat(isValid(valid_password)).isTrue();
+    }
+
+    @Property
+    void valid_lowercase(@ForAll @LowerChars @NotBlank String lower) {
+        assertThat(isValid(lower.repeat(5) + "A" + "1" + "*")).isTrue();
+    }
+
+    @Property
+    void valid_uppercase(@ForAll @UpperChars @NotBlank String upper) {
+        assertThat(isValid(upper.repeat(5) + "a" + "1" + "*")).isTrue();
+    }
+
+    @Property
+    void valid_number(@ForAll @NumericChars @NotBlank String number) {
+        assertThat(isValid(number.repeat(5) + "a" + "A" + "*")).isTrue();
+    }
+
+    @Property
+    void valid_special_character(@ForAll @Chars({'.', '*', '#', '@', '$', '%', '&'}) @NotBlank String special_character) {
+        assertThat(isValid(special_character.repeat(5) + "a" + "A" + "1")).isTrue();
     }
 
     @Test
     void less_than_8_characters() {
-        assertThat(isValid("ABCde1*")).isFalse();
+        assertThat(isValid(valid_password.substring(0, 7))).isFalse();
     }
 
     @Test
     void no_uppercase() {
-        assertThat(isValid("abcdef1*")).isFalse();
+        assertThat(isValid(valid_password.toLowerCase())).isFalse();
     }
 
     @Test
     void no_lowercase() {
-        assertThat(isValid("ABCDEF1*")).isFalse();
+        assertThat(isValid(valid_password.toUpperCase())).isFalse();
     }
 
     @Test
     void no_number() {
-        assertThat(isValid("ABCdefg*")).isFalse();
+        assertThat(isValid(valid_password.replace("1", "a"))).isFalse();
     }
 
     @Test
     void no_special_character() {
-        assertThat(isValid("ABCdef1h")).isFalse();
+        assertThat(isValid(valid_password.replace("*", "a"))).isFalse();
     }
 
     private boolean isValid(String password) {
