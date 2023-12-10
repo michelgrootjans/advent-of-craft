b171a3779bfa43a9e5a819f01f54c3b6a58f063c
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-10 13:38:12 +0100

    Renamed Foo to FizzBuzzRule

--- exercise/day10/src/main/java/games/FizzBuzz.java
+++ exercise/day10/src/main/java/games/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
     public static final int MIN = 0;
     public static final int MAX = 100;
 
-    private static final List<Foo> conditions = List.of(
+    private static final List<FizzBuzzRule> conditions = List.of(
         new RangeRule(MIN, MAX),
         new IfDivisibleBy(15, () -> "FizzBuzz"),
         new IfDivisibleBy(3, () -> "Fizz"),
@@ -19,18 +19,19 @@ public class FizzBuzz {
 
     public static String convert(Integer input) {
         return conditions.stream()
-            .filter(foo -> foo.matches(input))
+            .filter(rule -> rule.matches(input))
             .findFirst()
-            .map(Foo::bar).orElse(input.toString());
+            .map(FizzBuzzRule::execute)
+            .orElse(input.toString());
     }
 
-    private interface Foo {
+    private interface FizzBuzzRule {
         boolean matches(Integer input);
 
-        String bar();
+        String execute();
     }
 
-    private static class IfDivisibleBy implements Foo {
+    private static class IfDivisibleBy implements FizzBuzzRule {
         private final int divisor;
         private final Supplier<String> result;
 
@@ -45,12 +46,12 @@ public class FizzBuzz {
         }
 
         @Override
-        public String bar() {
+        public String execute() {
             return result.get();
         }
     }
 
-    private static class RangeRule implements Foo {
+    private static class RangeRule implements FizzBuzzRule {
         private final int min;
         private final int max;
 
@@ -65,7 +66,7 @@ public class FizzBuzz {
         }
 
         @Override
-        public String bar() {
+        public String execute() {
             throw new OutOfRangeException();
         }
 
