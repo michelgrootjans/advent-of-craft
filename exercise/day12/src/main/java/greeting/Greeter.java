a90ef4c47554d4038d45a69a0b91618ec10c3da5
Michel Grootjans <michel.grootjans@gmail.com>
2023-12-12 15:59:38 +0100

    day12: get rid of duplicate if-tree

--- exercise/day12/src/main/java/greeting/Greeter.java
+++ exercise/day12/src/main/java/greeting/Greeter.java
@@ -2,20 +2,9 @@ package greeting;
 
 public class Greeter {
     String formality;
-    IGreeter greeter;
+    private IGreeter greeter = new DefaultGreeter();
 
     public String greet() {
-        if (this.formality == null) {
-            return "Hello.";
-        }
-
-        if (this.formality.equals("formal")) {
-            return new FormalGreeter().greet();
-        } else if (this.formality.equals("casual")) {
-            return new CasualGreeter().greet();
-        } else if (this.formality.equals("intimate")) {
-            return new IntimateGreeter().greet();
-        }
         return greeter.greet();
     }
 
